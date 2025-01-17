c17254c2c730c15a13b435b8449eeb7e612eb200
Matúš Žilinec <zilinec.m@gmail.com>
2025-01-17 11:10:02 +0100

    Setup nvim

--- prepare_env.sh
+++ prepare_env.sh
@@ -3,3 +3,10 @@ apt install -y unzip zip less htop poppler-utils jq git
 
 git config --global user.email "zilinec.m@gmail.com"
 git config --global user.name "Matus Zilinec"
+
+# Setup nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+echo '' >> ~/.profile
+echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.profile
