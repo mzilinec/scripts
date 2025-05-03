04307ac670b80d507722daa81b737ddb6837cb76
Matúš Žilinec <zilinec.m@gmail.com>
2025-05-03 08:38:29 +0200

    Update prepare_env.sh

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # use sudo if necessary and support systems with no sudo
-command -v sudo &>/dev/null && alias no='sudo' || { [ "$(id -u)" -eq 0 ] && alias no='nop' || { echo "Error: sudo not available and not root"; exit 1; }; }
+command -v sudo &>/dev/null && alias no='sudo' || { [ "$(id -u)" -eq 0 ] && alias no='' || { echo "Error: sudo not available and not root"; exit 1; }; }
 
 no apt update
 no apt install -y unzip zip less htop poppler-utils jq git tmux
